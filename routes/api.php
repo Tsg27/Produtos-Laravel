5c4495ee27189c6c5ca885d01815bc8ca76a36a2
Tiago Soares <tsgo27@hotmail.com>
2025-01-31 18:16:51 -0300

    Config da rota métódo prático

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\ApiProdutos;
 use Illuminate\Support\Facades\Route;
 
 
+//Rota que trás todos os Metodo da controller ApiProdutos
+//Route::apiResource('produtos', 'App\Http\Controllers\ApiProdutos');
 
 Route::get('/produtos', [ApiProdutos::class, 'index']); //Exibir produtos
 Route::put('/produtos/{id}', [ApiProdutos::class, 'update']); // Atualizar produto
